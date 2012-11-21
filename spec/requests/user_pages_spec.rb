01b2c18128809618680ab297c3ba4c2ac2298267
Thomas Noh <thomasmaking@gmail.com>
2012-11-21 02:21:21 -0500

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -13,10 +13,10 @@ describe "User pages" do
 	end 
 
   	describe "signup page" do
-  	before { visit signup_path }
+  	 before { visit signup_path }
 
-  	it { should have_selector('h1',  text: 'Sign up') }
-  	it { should have_selector('title', text: full_title('Sign up')) }
+  	 it { should have_selector('h1',  text: 'Sign up') }
+  	 it { should have_selector('title', text: 'Sign up') }
   end
 
   describe "signup" do
@@ -30,6 +30,13 @@ describe "User pages" do
   		end
   	end
 
+    describe "after submission" do
+      before { click_button submit }
+
+      it { should have_selector('title', text: 'Sign up') }
+      it { should have_content('error') }
+    end
+
   	describe "with valid information" do
   		before do
   			fill_in "Name",				with: "Example User"
@@ -38,9 +45,17 @@ describe "User pages" do
   			fill_in "Confirmation",		with: "foobar"
   		end
 
+        describe "after saving the user" do
+          before { click_button submit }
+          let(:user) { User.find_by_email('user@example.com') }
+
+          it { should have_selector('title', text: user.name) }
+          it { should have_selector('div.alert.alert-success' text: 'Welcome') }
+        end
+
   		it "should create a user" do
   			expect { click_button submit }.to change(User, :count).by(1)
   		end
   	end
-  end
+  
 end
